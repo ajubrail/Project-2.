963e970e234711e056e9c01d932984639859786f
ajubrail <63420910+ajubrail@users.noreply.github.com>
2024-05-16 09:14:00 -0500

    Add files via upload

---
+++ week 2 #1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double capacity, MPG, total_miles;
+    cout << "Enter the capacity of the fuel tank in gallons: ";
+    cin >> capcity;
+
+    cout << "Enter the miles per gallon MPG of the automobile: ";
+    cin >> MPG;
+
+    total_miles = capacity * MPG;
+
+    cout << "The automobile can be driven for " << total_miles << " miles without refueling." << endl;
+
+    return 0;
+}
